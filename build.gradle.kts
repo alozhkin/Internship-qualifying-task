669123320376825cffaeadb73dd6c5a0ed43ac0e
andrey <alozhkin99@gmail.com>
2020-04-01 16:29:08 +0300

    implement calculate sha-1 plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,22 @@
 plugins {
-    kotlin("jvm") version "1.3.61"
+    kotlin("jvm") version "1.3.70"
 }
 
 repositories {
     jcenter()
+}
+
+buildscript {
+    repositories {
+        flatDir {
+            dirs("hash-sum-plugin/build/libs")
+        }
+    }
+    dependencies {
+        classpath("org.jetbrains.internship:hash-sum-plugin")
+    }
+}
+
+allprojects {
+    apply(plugin = "org.jetbrains.internship")
 }
--- hash-sum-plugin/src/main/kotlin/org/jetbrains/internship/HashSumPlugin.kt
+++ hash-sum-plugin/src/main/kotlin/org/jetbrains/internship/HashSumPlugin.kt
@@ -2,14 +2,14 @@ package org.jetbrains.internship
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.jetbrains.internship.tasks.HashTask
+import java.io.File
 
 class HashSumPlugin : Plugin<Project> {
     override fun apply(p0: Project) {
-
-        p0.tasks.create("calculateSha1") {
+        p0.tasks.create("calculateSha1", HashTask::class.java) {
+            outputFile.set(File("build/hash_sum.txt"))
+            fileExtensions.set(listOf("kt", "java"))
         }
-
-        //TODO replace with your implementation
-        println("HashingPlugin applied!")
     }
 }
